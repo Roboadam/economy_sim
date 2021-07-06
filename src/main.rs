ba23a6b8693be632bf51dde53f8439c6d5e4121e
Adam H <Roboadam@users.noreply.github.com>
2021-07-05 21:49:41 -0400

    more refactor

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,19 @@ use crate::land_mass_generator::create_land_mass;
 use macroquad::prelude::*;
 use selection::Selection;
 use tile_map::{TileMap, TileType};
-use tile_selector::TileSelector;
-use TileType::{Land, Sea};
+use TileType::Sea;
+use rendering::*;
 
 mod land_mass_generator;
 mod selection;
 mod tile_map;
 mod tile_selector;
+mod rendering;
 
 #[macroquad::main("Texture")]
 async fn main() {
-    let rt = render_target(2048, 2048);
-    rt.texture.set_filter(FilterMode::Nearest);
-
-    let texture_atlas: Texture2D = load_texture("textures/land_tilemap.png").await.unwrap();
-    texture_atlas.set_filter(FilterMode::Nearest);
+    let rt = pixel_perfect_render_target();
+    let texture_atlas = open_texture_atlas("textures/land_tilemap.png").await;
     const MAP_WIDTH_IN_TILES: usize = 160;
 
     let mut selection = Selection::new(MAP_WIDTH_IN_TILES);
@@ -29,36 +27,20 @@ async fn main() {
     loop {
         if is_key_pressed(KeyCode::W) {
             selection.up();
-            target.y = (selection.y * 16 + 126) as f32;
+            target.y = ((selection.y - 5) * 16 + 126) as f32;
         }
         if is_key_pressed(KeyCode::S) {
             selection.down();
-            target.y = (selection.y * 16 + 126) as f32;
+            target.y = ((selection.y - 5) * 16 + 126) as f32;
         }
         if is_key_pressed(KeyCode::A) {
             selection.left();
-            target.x = (selection.x * 16 + 126) as f32;
+            target.x = ((selection.x - 5) * 16 + 126) as f32;
         }
         if is_key_pressed(KeyCode::D) {
             selection.right();
-            target.x = (selection.x * 16 + 126) as f32;
+            target.x = ((selection.x - 5) * 16 + 126) as f32;
         }
-        // if is_key_down(KeyCode::Up) {
-        //     target.y -= 1.;
-        //     println!("target:{} selection:{:?}", target, selection)
-        // }
-        // if is_key_down(KeyCode::Down) {
-        //     target.y += 1.;
-        //     println!("target:{} selection:{:?}", target, selection)
-        // }
-        // if is_key_down(KeyCode::Left) {
-        //     target.x -= 1.;
-        //     println!("target:{} selection:{:?}", target, selection)
-        // }
-        // if is_key_down(KeyCode::Right) {
-        //     target.x += 1.;
-        //     println!("target:{} selection:{:?}", target, selection)
-        // }
         if is_key_pressed(KeyCode::C) {
             for y in 0..tile_map.width as i32 {
                 for x in 0..tile_map.width as i32 {
@@ -67,107 +49,13 @@ async fn main() {
             }
             create_land_mass(&mut tile_map);
         }
-        if is_key_pressed(KeyCode::Space) {
-            if let Some(tile_type) = tile_map.get_tile(selection.x, selection.y) {
-                let new_tile_type = match tile_type {
-                    Land => Sea,
-                    Sea => Land,
-                };
-                tile_map.set_tile(selection.x, selection.y, new_tile_type)
-            }
-        }
 
-        set_camera(&Camera2D {
-            zoom: vec2(0.008, 0.008),
-            target: target,
-            render_target: Some(rt),
-            ..Default::default()
-        });
-
+        draw_to_texture(rt, target);
         clear_background(LIGHTGRAY);
-
         draw_tile_map(&tile_map, 16., &texture_atlas);
         draw_selection(&selection, 16.);
-
-        set_default_camera();
-        clear_background(WHITE);
-        let max_width = if screen_height() > screen_width() {
-            screen_height()
-        } else {
-            screen_width()
-        };
-        draw_texture_ex(
-            rt.texture,
-            0.,
-            0.,
-            WHITE,
-            DrawTextureParams {
-                dest_size: Some(vec2(max_width, max_width)),
-                ..Default::default()
-            },
-        );
+        draw_texture_to_screen(rt);
 
         next_frame().await
-    }
-}
-
-pub fn draw_selection(selection: &Selection, tile_len: f32) {
-    draw_rectangle_lines(
-        selection.x as f32 * tile_len + 2.5,
-        selection.y as f32 * tile_len + 2.5,
-        tile_len - 5.,
-        tile_len - 5.,
-        5.,
-        WHITE,
-    );
-    draw_rectangle_lines(
-        selection.x as f32 * tile_len + 2.,
-        selection.y as f32 * tile_len + 2.,
-        tile_len - 4.,
-        tile_len - 4.,
-        3.,
-        BLACK,
-    );
-}
-
-pub fn draw_tile_map(tile_map: &TileMap, tile_width_in_screen_pixels: f32, texture_atlas: &Texture2D) {
-    let tile_selector = TileSelector::new();
-    for y in 0..tile_map.width as i32 {
-        for x in 0..tile_map.width as i32 {
-            draw_texture_ex(
-                *texture_atlas,
-                x as f32 * tile_width_in_screen_pixels,
-                y as f32 * tile_width_in_screen_pixels,
-                WHITE,
-                texture_params(x, y, tile_width_in_screen_pixels, tile_map, &tile_selector),
-            );
-        }
-    }
-}
-
-fn texture_params(
-    x: i32,
-    y: i32,
-    tile_len: f32,
-    tile_map: &TileMap,
-    tile_selector: &TileSelector,
-) -> DrawTextureParams {
-    const TILE_PIXEL_LEN: f32 = 16f32;
-    let nw = tile_map.get_tile(x, y).unwrap_or(&TileType::Sea);
-    let sw = tile_map.get_tile(x, y + 1).unwrap_or(&TileType::Sea);
-    let ne = tile_map.get_tile(x + 1, y).unwrap_or(&TileType::Sea);
-    let se = tile_map.get_tile(x + 1, y + 1).unwrap_or(&TileType::Sea);
-
-    let (x_coord, y_coord) = tile_selector.select_tile(*nw, *ne, *sw, *se);
-
-    DrawTextureParams {
-        source: Some(Rect {
-            x: TILE_PIXEL_LEN * x_coord as f32,
-            y: TILE_PIXEL_LEN * y_coord as f32,
-            w: TILE_PIXEL_LEN,
-            h: TILE_PIXEL_LEN,
-        }),
-        dest_size: Some(vec2(tile_len, tile_len)),
-        ..Default::default()
     }
 }
---
+++ src/rendering.rs
@@ -0,0 +1,106 @@
+use macroquad::prelude::*;
+
+use crate::{selection::Selection, tile_map::{TileMap, TileType}, tile_selector::TileSelector};
+
+pub fn pixel_perfect_render_target() -> RenderTarget {
+    let rt = render_target(2048, 2048);
+    rt.texture.set_filter(FilterMode::Nearest);
+    rt
+}
+
+pub fn draw_selection(selection: &Selection, tile_len: f32) {
+    draw_rectangle_lines(
+        selection.x as f32 * tile_len + 2.5,
+        selection.y as f32 * tile_len + 2.5,
+        tile_len - 5.,
+        tile_len - 5.,
+        5.,
+        WHITE,
+    );
+    draw_rectangle_lines(
+        selection.x as f32 * tile_len + 2.,
+        selection.y as f32 * tile_len + 2.,
+        tile_len - 4.,
+        tile_len - 4.,
+        3.,
+        BLACK,
+    );
+}
+
+pub fn draw_tile_map(tile_map: &TileMap, tile_width_in_screen_pixels: f32, texture_atlas: &Texture2D) {
+    let tile_selector = TileSelector::new();
+    for y in 0..tile_map.width as i32 {
+        for x in 0..tile_map.width as i32 {
+            draw_texture_ex(
+                *texture_atlas,
+                x as f32 * tile_width_in_screen_pixels,
+                y as f32 * tile_width_in_screen_pixels,
+                WHITE,
+                texture_params(x, y, tile_width_in_screen_pixels, tile_map, &tile_selector),
+            );
+        }
+    }
+}
+
+pub fn draw_to_texture(texture: RenderTarget, target: Vec2) {
+    set_camera(&Camera2D {
+        zoom: vec2(0.008, 0.008),
+        target: target,
+        render_target: Some(texture),
+        ..Default::default()
+    });
+}
+
+fn texture_params(
+    x: i32,
+    y: i32,
+    tile_len: f32,
+    tile_map: &TileMap,
+    tile_selector: &TileSelector,
+) -> DrawTextureParams {
+    const TILE_PIXEL_LEN: f32 = 16f32;
+    let nw = tile_map.get_tile(x, y).unwrap_or(&TileType::Sea);
+    let sw = tile_map.get_tile(x, y + 1).unwrap_or(&TileType::Sea);
+    let ne = tile_map.get_tile(x + 1, y).unwrap_or(&TileType::Sea);
+    let se = tile_map.get_tile(x + 1, y + 1).unwrap_or(&TileType::Sea);
+
+    let (x_coord, y_coord) = tile_selector.select_tile(*nw, *ne, *sw, *se);
+
+    DrawTextureParams {
+        source: Some(Rect {
+            x: TILE_PIXEL_LEN * x_coord as f32,
+            y: TILE_PIXEL_LEN * y_coord as f32,
+            w: TILE_PIXEL_LEN,
+            h: TILE_PIXEL_LEN,
+        }),
+        dest_size: Some(vec2(tile_len, tile_len)),
+        ..Default::default()
+    }
+}
+
+pub fn draw_texture_to_screen(texture: RenderTarget) {
+    set_default_camera();
+    clear_background(WHITE);
+    let max_width = if screen_height() > screen_width() {
+        screen_height()
+    } else {
+        screen_width()
+    };
+    draw_texture_ex(
+        texture.texture,
+        0.,
+        0.,
+        WHITE,
+        DrawTextureParams {
+            dest_size: Some(vec2(max_width, max_width)),
+            ..Default::default()
+        },
+    );
+}
+
+pub async fn open_texture_atlas(path: &str) -> Texture2D {
+    let texture_atlas = load_texture(path).await.unwrap();
+    texture_atlas.set_filter(FilterMode::Nearest);
+
+    texture_atlas
+}
