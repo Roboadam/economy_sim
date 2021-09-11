d04ddaf49a2cd45b5c59b52d7566c7f556d03964
Adam H <Roboadam@users.noreply.github.com>
2021-09-11 15:07:34 -0400

    cutting more stuff out

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use crate::ai_travel_point::{draw_travel_points, sample_travel_points};
 use crate::components::{AiPersonTag, Hunger, Position};
-use crate::person::{People, Person, PersonId};
 use crate::sprites::SpritePool;
 use ::rand::Rng;
 use ai_travel_point::AiTravelPoint;
@@ -13,29 +12,14 @@ use rendering::*;
 
 mod ai_travel_point;
 mod components;
-mod person;
 mod rendering;
 mod sprites;
 
 #[macroquad::main("City Sim")]
 async fn main() {
-    const SPEED: f32 = 50.;
-    let my_id = PersonId(0);
-    let player_texture = open_pixel_texture("textures/player.png").await;
     let ai_player_texture = open_pixel_texture("textures/ai_player.png").await;
     let mut rng = ChaCha8Rng::seed_from_u64(1);
 
-    let mut people = People::new();
-    people.add(
-        my_id,
-        Person {
-            hunger: 100.,
-            position: Position(0., 0.),
-        },
-    );
-
-    let mut curr_screen_width = screen_width() as i32;
-    let mut curr_screen_height = screen_height() as i32;
     let mut sprite_pool = SpritePool::new();
     let travel_point_sprite = sprite_pool.add("textures/ai_travel_point.png").await;
     let travel_points = sample_travel_points(travel_point_sprite);
@@ -50,36 +34,11 @@ async fn main() {
 
     loop {
         if is_key_pressed(KeyCode::F) {
-            println!(
-                "FPS: {}, player_coords: {:?}",
-                get_fps(),
-                people.get(my_id).position
-            );
-        }
-        if is_key_down(KeyCode::W) {
-            people.get_mut(my_id).position.1 -= SPEED * get_frame_time();
-        }
-        if is_key_down(KeyCode::S) {
-            people.get_mut(my_id).position.1 += SPEED * get_frame_time();
-        }
-        if is_key_down(KeyCode::A) {
-            people.get_mut(my_id).position.0 -= SPEED * get_frame_time();
-        }
-        if is_key_down(KeyCode::D) {
-            people.get_mut(my_id).position.0 += SPEED * get_frame_time();
-        }
-
-        if curr_screen_height != screen_height() as i32
-            || curr_screen_width != screen_width() as i32
-        {
-            curr_screen_width = screen_width() as i32;
-            curr_screen_height = screen_height() as i32;
+            println!("FPS: {}", get_fps());
         }
 
         clear_background(LIGHTGRAY);
-        let my_position = people.get(my_id).position;
         draw_travel_points(&travel_points, &sprite_pool);
-        draw_texture(player_texture, my_position.0, my_position.1, WHITE);
         hunger(&mut world, get_frame_time());
         travel(&mut world, &travel_points, get_frame_time(), &mut rng);
         draw_ai(&mut world, &ai_player_texture);
